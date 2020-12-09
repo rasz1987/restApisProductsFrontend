7524494397855a3578faf8864831650068abbadf
rasz1987 <rodolfosifontes1@gmail.com>
2020-12-09 18:12:35 +0000

    First configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import Header from './components/layout/Header';
+import Navegacion from './components/layout/Navegacion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          New react Project
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Fragment>
+            <Header />
+
+            <div class="grid contenedor contenido-principal">
+                <Navegacion />
+            </div>
+
+        </Fragment>
+        
+    )
 }
 
-export default App;
+export default App
