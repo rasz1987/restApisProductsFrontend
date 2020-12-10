348ee9487ba43baaaf17c8292e70b8c976498a07
rasz1987 <rodolfosifontes1@gmail.com>
2020-12-10 21:09:40 +0000

    Conif axios

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -1,6 +1,17 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react';
+import clienteAxios from '../../config/axios';
 
 const Clientes = () => {
+    
+    const consultarApi = async () => {
+        const clientesConsulta = await clienteAxios.get('/clientes');
+
+        console.log(clientesConsulta);
+    }
+
+    useEffect(() => {
+        consultarApi();
+    }, [])
     return (
         <Fragment>
             <h1>Componente clientes</h1>
